e28ffc460815657924084b4c93cbdd931e2b0029
franciscocasas <ffranciscocasas@gmail.com>
2021-09-13 07:48:06 -0300

    Commit message

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,23 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Article() {
-  return (
+class Article extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            image : " ",
+            titulo: " ",
+            descrip: " ",
+            popularidad: " ",
+            fecha: " ",
+            genero: " "
+        }
+    }
+
+    apiCall(url, consecuencia) {
+fetch(url)
+.then(res => res.json())
+.then(data => consecuencia(data))
+.catch(err => console.log(err))
+    }
+
+    componentDidMount() {
+this.apiCall("https://api.themoviedb.org/3/movie/upcoming?api_key=c57f4aacd06596fc7da4af9f1a6e8489", this.mostrarPelicula);
+    }
+    mostrarPelicula = (data) => {
+        console.log(data.results)
+        this.setState(
+            {image: "https://image.tmdb.org/t/p/w342/" + data.results[0].poster_path},
+            //{titulo:  data.results[0].title},
+            //{descrip: data.results[0].overview},
+            //{popularidad: data.results[0].popularity},
+            //{fecha: data.results[0].release_date },
+           // {genero: data.results[0].genre_ids},
+            )
+    
+    }
+
+    componentDidUpdate() {
+
+    }
+
+
+  render() {
+      return (
     <article>
-        <section class="navigation">
+        <section className="navigation">
             <div>
-                <i class="fas fa-chevron-left"></i>
-                <i class="fas fa-chevron-right"></i>
+                <i className="fas fa-chevron-left"></i>
+                <i className="fas fa-chevron-right"></i>
             </div>
-            <i class="far fa-window-close"></i>
+            <i className="far fa-window-close"></i>
         </section>
         <main>
-            <img src="../assets/img/image-default.png" alt=""/>
-            <h3>Título/ Nombre</h3>
-            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint cumque velit minus facere laboriosam voluptatem impedit ea unde labore optio eius quis, dignissimos expedita. Culpa, soluta perspiciatis! Sint, laboriosam cum.</p>
-            <section class="aditional-info">
-                <p><span className="infoAdicional">Info Adicional: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p>
-                <p><span className="infoAdicional">Info Adicional: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p>
-                <p><span className="infoAdicional">Info Adicional: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p>
+            <img src={this.state.image} alt=""/>
+            <h3>{this.state.titulo}</h3>
+            <p className="description">{this.state.descrip}</p>
+            <section className="aditional-info">
+                <p><span className="infoAdicional">Popularidad: </span>{this.state.popularidad}</p>
+                <p><span className="infoAdicional">Fecha de lanzamiento: </span>{this.state.fecha}</p>
+                <p><span className="infoAdicional">Geenero: </span>{this.state.genero}</p>
             </section>
             <div className='botonInfoAdicional'>
             <a href="">Ver más</a>
@@ -25,6 +66,7 @@ function Article() {
         </main>
     </article>
 );
+}
 }
 
 export default Article;
