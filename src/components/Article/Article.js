a6dca13dd2d27b8796366afbb59ab63c3e6ff10c
justipadullla <justina.padulla@gmail.com>
2021-09-16 14:16:42 -0300

    ver mas con errores

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -4,8 +4,24 @@ import React, { Component } from "react";
 class Article extends Component {
     constructor(props) {
         super(props);
+        this.state={
+        text:'ver mas',
+        viewMore:false,
+    }
     }
 
+    viewMore(){
+        if(this.state.viewMore === true){
+         this.setState({
+            text:'ver menos',
+            viewMore: false,
+         })
+        } else{
+            this.setState({
+            text:'ver mas',
+            viewMore: true,
+        })}
+    }
 
   render() {
       return (
@@ -15,16 +31,16 @@ class Article extends Component {
         </section>
         <main>
             <img src= {`https://image.tmdb.org/t/p/w342/${this.props.dataPelicula.poster_path}`} alt=""/>
-            <div className='botonInfoAdicional'>
+            <div className='botonInfoAdicional'onClick={()=>this.viewMore()}>
             <a href=""><img src='/assets/img/info.png.png'/></a>
             </div>
             <h3>{this.props.dataPelicula.title}</h3>
             <section className="aditional-info">
                 <p className="description">{this.props.dataPelicula.overview}</p>
                 <br/>
-                <p><span className="infoAdicional">Popularidad: </span>{this.props.dataPelicula.popularity}</p>
-                <p><span className="infoAdicional">Fecha de lanzamiento: </span>{this.props.dataPelicula.release_date}</p>
-                <p><span className="infoAdicional">Lenguaje: </span>{this.props.dataPelicula.original_language}</p>
+                <p className={`extra ${this.state.viewMore?'show':'hide'}`}><span className={`extra ${this.state.viewMore?'show':'hide'}`}>Popularidad: </span>{this.props.dataPelicula.popularity}</p>
+                <p className={`extra ${this.state.viewMore?'show':'hide'}`}><span className={`extra ${this.state.viewMore?'show':'hide'}`}>Fecha de lanzamiento: </span>{this.props.dataPelicula.release_date}</p>
+                <p className={`extra ${this.state.viewMore?'show':'hide'}`}><span className={`extra ${this.state.viewMore?'show':'hide'}`}>Lenguaje: </span>{this.props.dataPelicula.original_language}</p>
                 
             </section>
         </main>
